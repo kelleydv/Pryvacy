4fd6701d4fff12dce21feeaed92c9b56d20c9238
kelleydv <kelleydv@gmail.com>
2016-02-03 17:56:40 -0800

    more config, needed HEROKU env var on heroku

--- pryvacy/config.py
+++ pryvacy/config.py
@@ -3,12 +3,6 @@ import os
 class Config():
     SECRET_KEY = 'pryvacy'
 
-    MAIL_SERVER = 'smtp.dummy.com'
-    MAIL_PORT = '123'
-    MAIL_USERNAME = 'uname'
-    MAIL_PASSWORD = 'pass'
-    MAIL_DEFAULT_SENDER = 'awesome@awesome.com'
-
     HOST = 'http://localhost:5000'
 
 class Debug(Config):
@@ -17,7 +11,5 @@ class Debug(Config):
 
 class Develop(Debug):
     SECRET_KEY = os.environ.get('SECRET_KEY') or Config.SECRET_KEY
-
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-
     HOST = os.environ.get('pryvacy_host') or Config.HOST
+    
