eab6bb9c1dfcb0a5686011d8b8e0ac697db45e34
kelleydv <kelleydv@gmail.com>
2016-02-03 17:46:25 -0800

    unneeded config

--- pryvacy/config.py
+++ pryvacy/config.py
@@ -18,10 +18,6 @@ class Debug(Config):
 class Develop(Debug):
     SECRET_KEY = os.environ.get('SECRET_KEY') or Config.SECRET_KEY
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
 
     HOST = os.environ.get('pryvacy_host') or Config.HOST
